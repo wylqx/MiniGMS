7308fa6ecc624830325fc2c723c670c13c7a53d6
123 <123@123.com>
2024-06-28 16:16:47 +0800

    add fack store support

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 下载后，将google目录拷贝到/vendor/目录下，执行整包编译
 
 注意事项：
-google/Android.mk文件在中，USE_REAL_PLAYSTORE := true 时，将会在镜像中带上AndroidTV版本的谷歌商店（注意，谷歌商店可能会导致某些应用在进行GMS正确性校验时失败,从而无法运行）。
-如果您的镜像中不需要谷歌商店，请保持USE_REAL_PLAYSTORE := false
+google/Android.mk文件在中，USE_REAL_PLAYSTORE := GooglePlayStore 时，将会在镜像中带上AndroidTV版本的谷歌商店（注意，谷歌商店可能会导致某些应用在进行GMS正确性校验时失败,从而无法运行）；USE_REAL_PLAYSTORE := FackStore时，默认会使用定制版的应用商店，用户将无法自行安装谷歌应用商店；USE_REAL_PLAYSTORE := NoneStore时，默认不会安装任何应用商店，用户将无法自行安装谷歌应用商店。
--- google/Android.mk
+++ google/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(my-dir)
 
-USE_REAL_PLAYSTORE := false
+USE_REAL_PLAYSTORE := FackStore
 
 ##############################
 $(shell mkdir -p $(PRODUCT_OUT)/system)
@@ -29,12 +29,28 @@ include $(BUILD_PREBUILT)
 
 
 
-#if you want to use the real GooglePlayStore
-ifeq ($(USE_REAL_PLAYSTORE), true)
-    $(shell cp -rf $(LOCAL_PATH)/system/* $(PRODUCT_OUT)/system/ && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayServices.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GoogleServicesFramework.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-M.apk && mv $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-G.apk.bak $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-G.apk)
+# 根据 USE_REAL_PLAYSTORE 的值执行不同的代码
+ifeq ($(USE_REAL_PLAYSTORE), NoneStore)
+    $(shell cp -rf $(LOCAL_PATH)/system/* $(PRODUCT_OUT)/system/ && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayServices.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GoogleServicesFramework.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-M.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-G.apk.bak && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-A.apk)
 
-#if you want to use other PlayStore
-else
+    include $(CLEAR_VARS)
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE := GooglePlayStore
+    LOCAL_SRC_FILES := ./system/priv-app/GooglePlayStore-M.apk
+    LOCAL_MODULE_CLASS := APPS
+    LOCAL_BUILT_MODULE_STEM := package.apk
+    LOCAL_CERTIFICATE := platform
+    LOCAL_ENFORCE_USES_LIBRARIES := false
+    LOCAL_PRIVILEGED_MODULE := true
+    include $(BUILD_PREBUILT)
+endif
+
+#if you want to use the real GooglePlayStore
+ifeq ($(USE_REAL_PLAYSTORE), GooglePlayStore)
+    $(shell cp -rf $(LOCAL_PATH)/system/* $(PRODUCT_OUT)/system/ && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayServices.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GoogleServicesFramework.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-M.apk && mv $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-G.apk.bak $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-G.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-A.apk)
+endif
+
+ifeq ($(USE_REAL_PLAYSTORE), FackStore)
     $(shell cp -rf $(LOCAL_PATH)/system/* $(PRODUCT_OUT)/system/ && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayServices.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GoogleServicesFramework.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-M.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-G.apk.bak)
 
     include $(CLEAR_VARS)
