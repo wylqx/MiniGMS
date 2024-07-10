9e850d7d31ca67e2224e441683ab3d423e69cc7d
123 <123@123.com>
2024-07-10 16:39:47 +0800

    support FackStore

--- google/Android.mk
+++ google/Android.mk
@@ -51,12 +51,23 @@ ifeq ($(USE_REAL_PLAYSTORE), GooglePlayStore)
 endif
 
 ifeq ($(USE_REAL_PLAYSTORE), FackStore)
-    $(shell cp -rf $(LOCAL_PATH)/system/* $(PRODUCT_OUT)/system/ && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayServices.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GoogleServicesFramework.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-M.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-G.apk.bak)
+    $(shell cp -rf $(LOCAL_PATH)/system/* $(PRODUCT_OUT)/system/ && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayServices.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GoogleServicesFramework.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-M.apk && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-G.apk.bak && rm -rf $(PRODUCT_OUT)/system/priv-app/GooglePlayStore-A.apk)
 
     include $(CLEAR_VARS)
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE := GooglePlayStore
     LOCAL_SRC_FILES := ./system/priv-app/GooglePlayStore-M.apk
+    LOCAL_MODULE_CLASS := APPS
+    LOCAL_BUILT_MODULE_STEM := package.apk
+    LOCAL_CERTIFICATE := platform
+    LOCAL_ENFORCE_USES_LIBRARIES := false
+    LOCAL_PRIVILEGED_MODULE := true
+    include $(BUILD_PREBUILT)
+
+    include $(CLEAR_VARS)
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE := FackStore
+    LOCAL_SRC_FILES := ./system/priv-app/GooglePlayStore-A.apk
     LOCAL_MODULE_CLASS := APPS
     LOCAL_BUILT_MODULE_STEM := package.apk
     LOCAL_CERTIFICATE := platform
